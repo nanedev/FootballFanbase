fa3b7eae0aa077b41bf1d0521733d25bee2d334e
korisnik <malik.bisic@gmail.com>
2017-03-06 19:40:42 +0100

    fb i twitter buttoni

--- app/src/main/java/com/malikbisic/sportapp/LoginActivity.java
+++ app/src/main/java/com/malikbisic/sportapp/LoginActivity.java
@@ -2,12 +2,22 @@ package com.malikbisic.sportapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import io.fabric.sdk.android.Fabric;
 
 public class LoginActivity extends AppCompatActivity {
+
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "SGhHUU1SO947OcEmg9x0rHutI";
+    private static final String TWITTER_SECRET = "7dm7t0hRqgQzudePEiU9KLeovMfLfr4riKBHP64sTCnMH0WSVd";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+//        Fabric.with(this, new Twitter(authConfig));
         setContentView(R.layout.activity_login);
 
     }
