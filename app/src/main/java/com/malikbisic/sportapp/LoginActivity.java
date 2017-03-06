59bbbd6c18a7a7980e9e798dd231ef79821675b4
korisnik <malik.bisic@gmail.com>
2017-03-06 22:00:20 +0100

    register open activity

--- app/src/main/java/com/malikbisic/sportapp/LoginActivity.java
+++ app/src/main/java/com/malikbisic/sportapp/LoginActivity.java
@@ -1,15 +1,18 @@
 package com.malikbisic.sportapp;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.twitter.sdk.android.Twitter;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
 import io.fabric.sdk.android.Fabric;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
     private static final String TWITTER_KEY = "SGhHUU1SO947OcEmg9x0rHutI";
@@ -18,7 +21,7 @@ public class LoginActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
 
-
+    Button openRegisterActivityBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,14 +29,19 @@ public class LoginActivity extends AppCompatActivity {
 //        Fabric.with(this, new Twitter(authConfig));
         setContentView(R.layout.activity_login);
 
-        mAuth = FirebaseAuth.getInstance();
-        mAuthListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+       openRegisterActivityBtn = (Button) findViewById(R.id.registerBtn);
 
-            }
-        };
+        openRegisterActivityBtn.setOnClickListener(this);
 
 
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.registerBtn){
+            Intent openRegisterIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+            startActivity(openRegisterIntent);
+        }
     }
 }
