52e237c8e09562ab5288dbbb4dc7862e8bc09ad7
korisnik <malik.bisic@gmail.com>
2017-03-07 18:18:42 +0100

    kalendar ispis datum rodjenja

--- app/src/main/java/com/malikbisic/sportapp/RegisterActivity.java
+++ app/src/main/java/com/malikbisic/sportapp/RegisterActivity.java
@@ -6,17 +6,17 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.*;
+import android.widget.DatePicker;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Locale;
 
 public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
 
     EditText dateTx;
-    final int Date_Dialog_ID=0;
-    int cDay,cMonth,cYear;
-    Calendar cDate;
-    int sDay,sMonth,sYear;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,66 +28,34 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
     @Override
     public void onClick(View view) {
         if (view.getId() == R.id.dateRegLabel){
-            showDialog(Date_Dialog_ID);
-
+            new DatePickerDialog(RegisterActivity.this, date, myCalendar
+                    .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                    myCalendar.get(Calendar.DAY_OF_MONTH)).show();
         }
-
-    cDate=Calendar.getInstance();
-
-    cDay=cDate.get(Calendar.DAY_OF_MONTH);
-
-    cMonth=cDate.get(Calendar.MONTH);
-
-    cYear=cDate.get(Calendar.YEAR);
-
-    sDay=cDay;
-
-    sMonth=cMonth;
-
-    sYear=cYear;
-
-    updateDateDisplay(sYear,sMonth,sDay);
-
-        }
-
-    @Override
-    protected Dialog onCreateDialog(int id) {
-
-        switch (id) {
-
-            case Date_Dialog_ID:
-
-                return new DatePickerDialog(this, onDateSet, cYear, cMonth, cDay);
-
-        }
-
-        return null;
-
-    }
-    private void updateDateDisplay(int year,int month,int date) {
-
-        String date2 = date +" - " +(month+1)+" - "+year;
-        dateTx.setText(date2);
-
     }
 
-    private DatePickerDialog.OnDateSetListener onDateSet=new DatePickerDialog.OnDateSetListener() {
+    Calendar myCalendar = Calendar.getInstance();
+
+    DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
 
         @Override
-        public void onDateSet(android.widget.DatePicker datePicker, int year, int monthOfYear, int dayOfMonth) {
-            System.out.println("2");
+        public void onDateSet(DatePicker datePicker, int year, int monthOfYear,
+                              int dayOfMonth) {
+            // TODO Auto-generated method stub
+            myCalendar.set(Calendar.YEAR, year);
+            myCalendar.set(Calendar.MONTH, monthOfYear);
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
 
-            sYear=year;
-
-            sMonth=monthOfYear;
-
-            sDay=dayOfMonth;
-
-            updateDateDisplay(sYear,sMonth,sDay);
+            updateLabel();
         }
-
-
 
     };
 
+    private void updateLabel() {
+
+        String myFormat = "dd/MMMM/yyyy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.ENGLISH);
+
+        dateTx.setText(sdf.format(myCalendar.getTime()));
+    }
 }
