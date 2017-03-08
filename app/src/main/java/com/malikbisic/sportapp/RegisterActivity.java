7ec4103fc0edfe6915857e464b31d3d0bfe91af2
alendev <tutarisalen@gmail.com>
2017-03-08 23:22:03 +0100

    initial commit

--- app/src/main/java/com/malikbisic/sportapp/RegisterActivity.java
+++ app/src/main/java/com/malikbisic/sportapp/RegisterActivity.java
@@ -34,13 +34,9 @@ import java.util.Calendar;
 import java.util.List;
 import java.util.Locale;
 
-public class RegisterActivity extends AppCompatActivity implements View.OnClickListener, View.OnKeyListener {
-  /*  private EditText nameField;
-    private EditText nickField;
-    private EditText emailField;
-    private EditText passwordField;
-    private ConstraintLayout registerLayout;*/
-  private static final String TAG = "SignupActivity";
+public class RegisterActivity extends AppCompatActivity {
+
+    private static final String TAG = "SignupActivity";
     EditText mNameText;
     EditText mSurnameText;
     EditText mEmailText;
@@ -90,6 +86,10 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         mLoginLink = (TextView) findViewById(R.id.link_login);
 
 
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance();
+
+
         mSignupButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -101,47 +101,15 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
             @Override
             public void onClick(View v) {
                 // Finish the registration screen and return to the Login activity
-                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
                 startActivity(intent);
                 finish();
                 overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
             }
         });
 
-
-
-
-
-
-
-
-
-      /*  dateTx = (EditText) findViewById(R.id.dateRegLabel);
-        dateTx.setOnClickListener(this);
-        spinnerArray = new ArrayList<>();
-        spinnerArray.add("Male");
-        spinnerArray.add("Female");
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, spinnerArray);
-
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        genderItems = (Spinner) findViewById(R.id.spinnerRegGender);
-        genderItems.setAdapter(adapter);
-
-        mAuth = FirebaseAuth.getInstance();
-        mDialog = new ProgressDialog(this);
-        mDatabase = FirebaseDatabase.getInstance();
-
-        registerLayout.setOnClickListener(this);
-
-        regBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                registerUser();
-            }
-        });
-
-*/
     }
+
     public void signup() {
         Log.d(TAG, "Signup");
 
@@ -158,15 +126,48 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         progressDialog.setMessage("Creating Account...");
         progressDialog.show();
 
-        String name = mNameText.getText().toString();
-        String surname = mSurnameText.getText().toString();
-        String email = mEmailText.getText().toString();
-        String nick = mNickNameText.getText().toString();
-        String password = mPasswordText.getText().toString();
-        String reEnterPassword = mReEnterPasswordText.getText().toString();
+        userName = mNameText.getText().toString();
+        userSurname = mSurnameText.getText().toString();
+        userEmail = mEmailText.getText().toString();
+        userNick = mNickNameText.getText().toString();
+        userPassword = mPasswordText.getText().toString();
+
 
         // TODO: Implement your own signup logic here.
-        registerUser();
+        if (!TextUtils.isEmpty(userName) && !TextUtils.isEmpty(userSurname) && !TextUtils.isEmpty(userNick) && !TextUtils.isEmpty(userEmail)
+                && !TextUtils.isEmpty(userPassword)) {
+
+
+
+            mAuth.createUserWithEmailAndPassword(userEmail, userPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+
+                    if (task.isSuccessful()) {
+                        user_id = mAuth.getCurrentUser().getUid();
+
+                        mReference = mDatabase.getReference().child("Users").child(user_id);
+                        mReference.child("name").setValue(userName);
+                        mReference.child("surname").setValue(userSurname);
+                        mReference.child("nick").setValue(userNick);
+                        /*mReference.child("date").setValue(userDate);
+                        mReference.child("gender").setValue(userGender);*/
+
+                        mReference = mDatabase.getReference().child("Nickname").push();
+                        mReference.setValue(userNick);
+
+
+                    }
+
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    e.printStackTrace();
+
+                }
+            });
+        }
 
         new android.os.Handler().postDelayed(
                 new Runnable() {
@@ -178,7 +179,7 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
                         progressDialog.dismiss();
                     }
                 }, 3000);
-        }
+    }
 
     public void onSignupSuccess() {
         mSignupButton.setEnabled(true);
@@ -210,7 +211,7 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         }
 
         if (surname.isEmpty()) {
-            mSurnameText.setError("Enter Surname");
+            mSurnameText.setError("Can't left blank");
             valid = false;
         } else {
             mSurnameText.setError(null);
@@ -225,20 +226,20 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         }
 
         if (nick.isEmpty()) {
-            mNickNameText.setError("Nick already exists");
+            mNickNameText.setError("Can't be blank");
             valid = false;
         } else {
             mNickNameText.setError(null);
         }
 
         if (password.isEmpty() || password.length() < 6 || password.length() > 10) {
-            mPasswordText.setError("between 6 and 10 alphanumeric characters");
+            mPasswordText.setError("password has to be atleast 6 characters");
             valid = false;
         } else {
             mPasswordText.setError(null);
         }
 
-        if (reEnterPassword.isEmpty() || reEnterPassword.length() < 6|| reEnterPassword.length() > 10 || !(reEnterPassword.equals(password))) {
+        if (reEnterPassword.isEmpty() || reEnterPassword.length() < 6 || reEnterPassword.length() > 10 || !(reEnterPassword.equals(password))) {
             mReEnterPasswordText.setError("Password Do not match");
             valid = false;
         } else {
@@ -248,9 +249,14 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         return valid;
     }
 
-    @Override
+
+}
+
+
+
+/* @Override
     public void onClick(View view) {
-      /*  if (view.getId() == R.id.dateRegLabel) {
+      *//*  if (view.getId() == R.id.dateRegLabel) {
             new DatePickerDialog(RegisterActivity.this, date, myCalendar
                     .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
                     myCalendar.get(Calendar.DAY_OF_MONTH)).show();
@@ -261,68 +267,24 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         } else if (view.getId() == R.id.registerLayout){
             InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
             imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
-        }*/
+        }*//*
     }
 
-    private void registerUser() {
+    *//*private void registerUser() {
         userName = mNameText.getText().toString().trim();
         userSurname = mSurnameText.getText().toString().trim();
         userNick = mNickNameText.getText().toString().trim();
 
         userEmail = mEmailText.getText().toString().trim();
         userPassword = mPasswordText.getText().toString().trim();
-       /* userDate = dateTx.getText().toString().trim();
-        userGender = genderItems.getSelectedItem().toString().trim();*/
+       *//**//* userDate = dateTx.getText().toString().trim();
+        userGender = genderItems.getSelectedItem().toString().trim();*//**//*
 
 
 
-        if (!TextUtils.isEmpty(userName) && !TextUtils.isEmpty(userSurname) &&!TextUtils.isEmpty(userNick) && !TextUtils.isEmpty(userEmail)
-                && !TextUtils.isEmpty(userPassword) )
-        {
-            mDialog.setMessage("Registering");
-            mDialog.show();
 
 
-            mAuth.createUserWithEmailAndPassword(userEmail,userPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                @Override
-                public void onComplete(@NonNull Task<AuthResult> task) {
-
-                    if (task.isSuccessful()){
-                       user_id = mAuth.getCurrentUser().getUid();
-                        mReference= mDatabase.getReference().child("Users").child(user_id);
-                        mReference.child("name").setValue(userName);
-                        mReference.child("name").setValue(userSurname);
-                        mReference.child("nick").setValue(userNick);
-                        /*mReference.child("date").setValue(userDate);
-                        mReference.child("gender").setValue(userGender);*/
-
-                        mReference = mDatabase.getReference().child("Nickname").push();
-                        mReference.setValue(userNick);
-
-                        mDialog.dismiss();
-
-
-
-                    }
-
-                }
-            }).addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception e) {
-
-                   /* if (TextUtils.isEmpty(userPassword) || userPassword.length() < 6){
-                        passwordField.setError("Password must be at least 6 characters");
-                        mDialog.dismiss();
-                    }
-
-                    emailField.setError(e.getMessage());
-                    mDialog.dismiss();
-*/
-                }
-            });
-        }
-
-        /*if (TextUtils.isEmpty(userName)) {
+        *//**//*if (TextUtils.isEmpty(userName)) {
             nameField.setError("You did not enter a name");
             mDialog.dismiss();
         }
@@ -335,11 +297,11 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
             dateTx.setError("You did not enter a birthday");
             mDialog.dismiss();
         }
-*/
+*//**//*
 
     }
 
-   /* Calendar myCalendar = Calendar.getInstance();
+   *//**//* Calendar myCalendar = Calendar.getInstance();
 
     DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
 
@@ -354,23 +316,49 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
             updateLabel();
         }
 
-    };*/
+    };*//*
 
     private void updateLabel() {
 
-       /* String myFormat = "dd/MMMM/yyyy"; //In which you need put here
+       *//* String myFormat = "dd/MMMM/yyyy"; //In which you need put here
         SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.ENGLISH);
 
-        dateTx.setText(sdf.format(myCalendar.getTime()));*/
+        dateTx.setText(sdf.format(myCalendar.getTime()));*//*
     }
 
     @Override
     public boolean onKey(View view, int i, KeyEvent keyEvent) {
 
         if (i == KeyEvent.KEYCODE_ENTER){
-            registerUser();
+            *//*registerUser();*//*
         }
 
         return false;
-    }
-}
+    }*/
+
+
+      /*  dateTx = (EditText) findViewById(R.id.dateRegLabel);
+        dateTx.setOnClickListener(this);
+        spinnerArray = new ArrayList<>();
+        spinnerArray.add("Male");
+        spinnerArray.add("Female");
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, spinnerArray);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        genderItems = (Spinner) findViewById(R.id.spinnerRegGender);
+        genderItems.setAdapter(adapter);
+
+
+        mDialog = new ProgressDialog(this);
+        mDatabase = FirebaseDatabase.getInstance();
+
+        registerLayout.setOnClickListener(this);
+
+        regBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registerUser();
+            }
+        });
+
+*/
