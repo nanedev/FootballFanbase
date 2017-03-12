ede509af03073a61771ec7b6bfbdaf0e913951c7
alendev <tutarisalen@gmail.com>
2017-03-12 15:30:28 +0100

    nick compare not finished

--- app/src/main/java/com/malikbisic/sportapp/RegisterActivity.java
+++ app/src/main/java/com/malikbisic/sportapp/RegisterActivity.java
@@ -24,12 +24,15 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -478,7 +481,7 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
 
     @Override
     public void afterTextChanged(Editable s) {
-        mReference.child("Users").orderByChild("nick").equalTo(mNickNameText.getText().toString())
+      /*  mReference.child("Users").orderByChild("nick").equalTo(mNickNameText.getText().toString())
                 .addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
                     public void onDataChange(DataSnapshot dataSnapshot) {
@@ -494,7 +497,7 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
                         nicknameExists.set(true);
                         checkingNick.set(false);
                     }
-                });
+                });*/
         validate();
     }
 
@@ -502,6 +505,41 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
     public void onStart() {
         super.onStart();
         mAuth.addAuthStateListener(mAuthListener);
+        final ArrayList<String> nickList = new ArrayList<>();
+
+
+        mReference = mDatabase.getReference("Users");
+
+        mReference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                nickList.add(dataSnapshot.getValue().toString());
+                if (nickList.contains(mNickNameText.getText().toString())) {
+                    mNameText.setError("already exists");
+                }
+                Log.i("proba", nickList.toString());
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 
     @Override
