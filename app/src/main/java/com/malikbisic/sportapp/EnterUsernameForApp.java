7115554f7fa4b1b0996ec6317eaa353260006308
alendev <tutarisalen@gmail.com>
2017-03-20 18:55:05 +0100

    enter username layout

--- app/src/main/java/com/malikbisic/sportapp/EnterUsernameForApp.java
+++ app/src/main/java/com/malikbisic/sportapp/EnterUsernameForApp.java
@@ -8,6 +8,6 @@ public class EnterUsernameForApp extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_set_up_acc_from_google);
+        setContentView(R.layout.enter_username);
     }
 }
