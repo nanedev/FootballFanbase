1e52fda9e91cdd25c1bfc2a436d9918fe5407e63
alendev <tutarisalen@gmail.com>
2017-03-22 14:52:15 +0100

    Parse

--- app/src/main/java/com/malikbisic/sportapp/Application.java
+++ app/src/main/java/com/malikbisic/sportapp/Application.java
@@ -1,6 +1,7 @@
 package com.malikbisic.sportapp;
 
 import com.google.firebase.database.FirebaseDatabase;
+import com.parse.Parse;
 
 /**
  * Created by Nane on 16.3.2017.
@@ -12,6 +13,11 @@ public class Application extends android.app.Application {
     public void onCreate() {
         super.onCreate();
         FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("1l5MQohP0oGkgU2LhfK89mRV6ojjQYFXc7CQwvw1")
+                .server("https://parseapi.back4app.com/")
+                .build()
+        );
 
     }
 }
--- app/src/main/java/com/malikbisic/sportapp/EnterUsernameForApp.java
+++ app/src/main/java/com/malikbisic/sportapp/EnterUsernameForApp.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.util.DiffUtil;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
@@ -86,4 +87,6 @@ public class EnterUsernameForApp extends AppCompatActivity implements View.OnCli
         }
 
     }
+
+
 }
