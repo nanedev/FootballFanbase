2a2310f17912fd1554aa2bb012153f273c92e40f
alendev <tutarisalen@gmail.com>
2017-04-24 18:02:18 +0200

    video view

--- app/src/main/java/com/malikbisic/sportapp/AddPhotoOrVideo.java
+++ app/src/main/java/com/malikbisic/sportapp/AddPhotoOrVideo.java
@@ -1,13 +1,19 @@
 package com.malikbisic.sportapp;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.media.MediaPlayer;
+
 import android.net.Uri;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.MediaController;
 import android.widget.VideoView;
 
 public class AddPhotoOrVideo extends AppCompatActivity {
@@ -17,6 +23,7 @@ public class AddPhotoOrVideo extends AppCompatActivity {
     private VideoView videoSelected;
     private EditText saySomething;
     private Button post;
+    ProgressDialog pDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +35,7 @@ public class AddPhotoOrVideo extends AppCompatActivity {
         saySomething = (EditText) findViewById(R.id.tell_something_about_video_image);
         post = (Button) findViewById(R.id.btn_post_photo_or_video);
 
+
         if (MainPage.photoSelected){
             photoSelected.setVisibility(View.VISIBLE);
             videoSelected.setVisibility(View.GONE);
@@ -35,11 +43,38 @@ public class AddPhotoOrVideo extends AppCompatActivity {
 
             Uri imageUri = Uri.parse(myIntent.getStringExtra("image-uri_selected"));
             photoSelected.setImageURI(imageUri);
-        } else if (MainPage.photoSelected == false){
+        } else if (!MainPage.photoSelected) {
             photoSelected.setVisibility(View.GONE);
             videoSelected.setVisibility(View.VISIBLE);
-            Uri videoUri = Uri.parse(myIntent.getStringExtra("video-uri_selected"));
-            videoSelected.setVideoURI(videoUri);
+            pDialog = new ProgressDialog(this);
+            pDialog.setMessage("Buffering");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(false);
+            pDialog.show();
+
+            try {
+                // Start the MediaController
+                MediaController mediacontroller = new MediaController(
+                        AddPhotoOrVideo.this);
+                mediacontroller.setAnchorView(videoSelected);
+                // Get the URL from String VideoURL
+                Uri video = Uri.parse(myIntent.getStringExtra("video-uri_selected"));
+                videoSelected.setMediaController(mediacontroller);
+                videoSelected.setVideoURI(video);
+
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+
+            videoSelected.requestFocus();
+            videoSelected.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                // Close the progress bar and play the video
+                public void onPrepared(MediaPlayer mp) {
+                    pDialog.dismiss();
+                    videoSelected.start();
+                }
+            });
 
         }
 
