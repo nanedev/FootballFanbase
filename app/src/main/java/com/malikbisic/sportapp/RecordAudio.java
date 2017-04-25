f281fa820197c3ac40badc00c9edb16f8d7b00fd
alendev <tutarisalen@gmail.com>
2017-04-25 19:07:05 +0200

    record audio activity

--- app/src/main/java/com/malikbisic/sportapp/RecordAudio.java
+++ app/src/main/java/com/malikbisic/sportapp/RecordAudio.java
@@ -1,9 +1,13 @@
 package com.malikbisic.sportapp;
 
 import android.app.ProgressDialog;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.net.Uri;
 import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,101 +15,242 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageMetadata;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
+import com.twitter.sdk.android.core.models.TwitterCollection;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.Date;
+import java.util.Random;
 
-public class RecordAudio extends AppCompatActivity implements View.OnTouchListener {
-    Button holdToRecordBtn;
-    TextView recordingState;
-    private MediaRecorder mRecorder;
-    String mFileName = null;
-    StorageReference audioReference;
-    StorageReference filePath;
-    private ProgressDialog mDialog;
+import static android.Manifest.permission.RECORD_AUDIO;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
+public class RecordAudio extends AppCompatActivity {
+    Button buttonStart, buttonStop, buttonPlayLastRecordAudio,
+            buttonStopPlayingRecording, buttonUploadAudio, buttonPost;
+    String AudioSavePathInDevice = null;
+    MediaRecorder mediaRecorder;
+    Random random;
+    String RandomAudioFileName = "ABCDEFGHIJKLMNOP";
+    public static final int RequestPermissionCode = 1;
+    MediaPlayer mediaPlayer;
+    StorageReference mStorage;
+    ProgressDialog mDialog;
 
     private static final String LOG_TAG = "record_log";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record_audio);
-        holdToRecordBtn = (Button) findViewById(R.id.record_voice_btn);
-        recordingState = (TextView) findViewById(R.id.record_state);
-
+        buttonStart = (Button) findViewById(R.id.button);
+        buttonStop = (Button) findViewById(R.id.button2);
+        buttonPlayLastRecordAudio = (Button) findViewById(R.id.button3);
+        buttonStopPlayingRecording = (Button) findViewById(R.id.button4);
+        buttonUploadAudio = (Button) findViewById(R.id.upload_audio);
+        buttonPost = (Button) findViewById(R.id.post_btn);
         mDialog = new ProgressDialog(this);
-        Date createdTime = new Date();
 
-        mFileName = Environment.getExternalStorageDirectory().getAbsolutePath();
-        mFileName += "/"+createdTime+"_recorded_audio.3gp";
+        buttonStop.setEnabled(false);
+        buttonPlayLastRecordAudio.setEnabled(false);
+        buttonStopPlayingRecording.setEnabled(false);
+        buttonUploadAudio.setEnabled(false);
+        buttonPost.setEnabled(false);
+        mStorage = FirebaseStorage.getInstance().getReference();
 
-        holdToRecordBtn.setOnTouchListener(this);
+        random = new Random();
 
-        audioReference = FirebaseStorage.getInstance().getReference();
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (checkPermission()) {
+
+                    AudioSavePathInDevice =
+                            Environment.getExternalStorageDirectory().getAbsolutePath() + "/" +
+                                    CreateRandomAudioFileName(5) + "AudioRecording.3gp";
+
+                    MediaRecorderReady();
+
+                    try {
+                        mediaRecorder.prepare();
+                        mediaRecorder.start();
+                    } catch (IllegalStateException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+
+                    buttonStart.setEnabled(false);
+                    buttonStop.setEnabled(true);
+                    buttonPlayLastRecordAudio.setEnabled(false);
+                    buttonUploadAudio.setEnabled(false);
+                    buttonPost.setEnabled(false);
+
+                    Toast.makeText(RecordAudio.this, "Recording started",
+                            Toast.LENGTH_LONG).show();
+                } else {
+                    requestPermission();
+                }
+
+            }
+        });
+
+        buttonStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaRecorder.stop();
+                buttonStop.setEnabled(false);
+                buttonPlayLastRecordAudio.setEnabled(true);
+                buttonStart.setEnabled(true);
+                buttonStopPlayingRecording.setEnabled(false);
+                buttonUploadAudio.setEnabled(true);
+                buttonPost.setEnabled(false);
+
+                Toast.makeText(RecordAudio.this, "Recording Completed",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+        buttonPlayLastRecordAudio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) throws IllegalArgumentException,
+                    SecurityException, IllegalStateException {
+
+                buttonStop.setEnabled(false);
+                buttonStart.setEnabled(false);
+                buttonStopPlayingRecording.setEnabled(true);
+                buttonUploadAudio.setEnabled(false);
+                buttonPost.setEnabled(false);
+
+                mediaPlayer = new MediaPlayer();
+                try {
+                    mediaPlayer.setDataSource(AudioSavePathInDevice);
+                    mediaPlayer.prepare();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                mediaPlayer.start();
+                Toast.makeText(RecordAudio.this, "Recording Playing",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+        buttonStopPlayingRecording.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                buttonStop.setEnabled(false);
+                buttonStart.setEnabled(true);
+                buttonStopPlayingRecording.setEnabled(false);
+                buttonPlayLastRecordAudio.setEnabled(true);
+                buttonUploadAudio.setEnabled(true);
+                buttonPost.setEnabled(false);
+
+                if (mediaPlayer != null) {
+                    mediaPlayer.stop();
+                    mediaPlayer.release();
+                    MediaRecorderReady();
+                }
+            }
+        });
+
+        buttonUploadAudio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonStart.setEnabled(false);
+                buttonPlayLastRecordAudio.setEnabled(false);
+                buttonStop.setEnabled(false);
+                buttonStopPlayingRecording.setEnabled(false);
+
+                mDialog.setMessage("Uploading...");
+                mDialog.show();
+                StorageMetadata metadata = new StorageMetadata.Builder()
+                        .setContentType("audio/3GPP")
+                        .build();
+                StorageReference filePath = mStorage.child("Audio").child(CreateRandomAudioFileName(5));
+                Uri uri = Uri.fromFile(new File(AudioSavePathInDevice));
+                filePath.putFile(uri, metadata).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        mDialog.dismiss();
+                        buttonStart.setEnabled(true);
+                        buttonStopPlayingRecording.setEnabled(false);
+                        buttonStop.setEnabled(false);
+                        buttonPlayLastRecordAudio.setEnabled(false);
+                        buttonPost.setEnabled(true);
+                        buttonUploadAudio.setEnabled(false);
+                    }
+                });
+
+            }
+        });
 
     }
 
+    public void MediaRecorderReady() {
+        mediaRecorder = new MediaRecorder();
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mediaRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
+        mediaRecorder.setOutputFile(AudioSavePathInDevice);
+    }
 
-    private void startRecording() {
-        mRecorder = new MediaRecorder();
-        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-        mRecorder.setOutputFile(mFileName);
-        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+    public String CreateRandomAudioFileName(int string) {
+        StringBuilder stringBuilder = new StringBuilder(string);
+        int i = 0;
+        while (i < string) {
+            stringBuilder.append(RandomAudioFileName.
+                    charAt(random.nextInt(RandomAudioFileName.length())));
 
-        try {
-            mRecorder.prepare();
-        } catch (IOException e) {
-            Log.e(LOG_TAG, "prepare() failed");
+            i++;
         }
-
-        mRecorder.start();
+        return stringBuilder.toString();
     }
 
-    private void stopRecording() {
-        mRecorder.stop();
-        mRecorder.release();
-        mRecorder = null;
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(RecordAudio.this, new
+                String[]{WRITE_EXTERNAL_STORAGE, RECORD_AUDIO}, RequestPermissionCode);
     }
 
     @Override
-    public boolean onTouch(View v, MotionEvent event) {
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case RequestPermissionCode:
+                if (grantResults.length > 0) {
+                    boolean StoragePermission = grantResults[0] ==
+                            PackageManager.PERMISSION_GRANTED;
+                    boolean RecordPermission = grantResults[1] ==
+                            PackageManager.PERMISSION_GRANTED;
 
-        if (v.getId() == R.id.record_voice_btn) {
-
-            if (event.getAction() == MotionEvent.ACTION_DOWN) {
-
-                startRecording();
-                recordingState.setText("Recording started....");
-
-            } else if (event.getAction() == MotionEvent.ACTION_UP) {
-
-                stopRecording();
-                recordingState.setText("Recording stopped!");
-                uploadAudio();
-            }
-
+                    if (StoragePermission && RecordPermission) {
+                        Toast.makeText(RecordAudio.this, "Permission Granted",
+                                Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(RecordAudio.this, "Permission Denied", Toast.LENGTH_LONG).show();
+                    }
+                }
+                break;
         }
-
-
-        return false;
     }
 
-    private void uploadAudio() {
-        mDialog.setMessage("Uploading audio...");
-        mDialog.show();
-        Uri uri = Uri.fromFile(new File(mFileName));
-        filePath = audioReference.child("Audio").child(uri.getLastPathSegment());
-        filePath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-            @Override
-            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                mDialog.dismiss();
-            }
-        });
+    public boolean checkPermission() {
+        int result = ContextCompat.checkSelfPermission(getApplicationContext(),
+                WRITE_EXTERNAL_STORAGE);
+        int result1 = ContextCompat.checkSelfPermission(getApplicationContext(),
+                RECORD_AUDIO);
+        return result == PackageManager.PERMISSION_GRANTED &&
+                result1 == PackageManager.PERMISSION_GRANTED;
     }
+
+
 }
