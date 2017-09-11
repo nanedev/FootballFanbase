840ec9557f72329a0a9f0db823d18796ebffc081
alendev <tutarisalen@gmail.com>
2017-09-11 17:51:55 +0200

    ic plus added chat layout not finished

--- app/src/main/java/com/malikbisic/sportapp/activity/ChatMessageActivity.java
+++ app/src/main/java/com/malikbisic/sportapp/activity/ChatMessageActivity.java
@@ -1,21 +1,71 @@
 package com.malikbisic.sportapp.activity;
 
+import android.content.Context;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
 
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.malikbisic.sportapp.R;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ChatMessageActivity extends AppCompatActivity {
     private String mChatUser;
-private Toolbar mChatToolbar;
+    private Toolbar mChatToolbar;
+    private DatabaseReference mRootRef;
+    String mChatUsername;
+    TextView mTitleView;
+    TextView mLastSeenView;
+    CircleImageView mProfileImg;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_message);
-
-
+mRootRef = FirebaseDatabase.getInstance().getReference();
+        mChatToolbar = (Toolbar) findViewById(R.id.chat_toolbar);
+        setSupportActionBar(mChatToolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowCustomEnabled(true);
 
         mChatUser = getIntent().getStringExtra("userId");
+        mChatUsername = getIntent().getStringExtra("username");
+       // getSupportActionBar().setTitle(mChatUsername);
+
+        LayoutInflater inflater = (LayoutInflater)this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View action_bar_view = inflater.inflate(R.layout.chat_custom_bar,null);
+        actionBar.setCustomView(action_bar_view);
+
+        mTitleView = (TextView) findViewById(R.id.chat_username);
+        mLastSeenView = (TextView) findViewById(R.id.chat_last_seen);
+        mProfileImg = (CircleImageView) findViewById(R.id.chat_imageview_id);
+
+        mTitleView.setText(mChatUsername);
+        mRootRef.child("Users").child(mChatUser).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                String online = dataSnapshot.child("online").toString();
+                String image = dataSnapshot.child("profileImage").toString();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/malikbisic/sportapp/application/Application.java
+++ app/src/main/java/com/malikbisic/sportapp/application/Application.java
@@ -14,6 +14,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ServerValue;
 import com.google.firebase.database.ValueEventListener;
 import com.malikbisic.sportapp.activity.MainPage;
 import com.parse.Parse;
@@ -69,8 +70,12 @@ public class Application extends android.app.Application{
                     mUsers.addValueEventListener(new ValueEventListener() {
                         @Override
                         public void onDataChange(DataSnapshot dataSnapshot) {
-                            mUsers.child("online").onDisconnect().setValue(false);
+                            mUsers.child("online").onDisconnect().setValue(ServerValue.TIMESTAMP);
                             mUsers.child("online").setValue(true);
+
+
+
+
                         }
 
                         @Override
